5350ddc063d2e616580e28c145092e878c7fc4f8
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-10 18:17:42 +0200

    aggiunta ciclo stampa img box dx aggiunta prima modifica opacity al click

--- js/main.js
+++ js/main.js
@@ -17,26 +17,55 @@ for (i = 0; i < imgArrey.length; i++){
     container.append(newDiv);
 }
 
+
+// Ciclo per carosello dx
+const boxDx = document.getElementById("right_box")
+
+for (i = 0; i < imgArrey.length; i++){
+    itemImg = imgArrey[i];
+    // Creazione div contenitore diretto immagini
+    let newDiv = document.createElement("div");
+    // Stampa di immagine a ogni giro
+    newDiv.innerHTML += `<img src="${itemImg}" alt="img${i}">`;
+    newDiv.className = "box_img_dx";
+    boxDx.append(newDiv);
+}
+
 // Selezione di tutte le img all'interno del carosello
 const itemsArrey = document.getElementsByClassName("box_carosel");
+// Selezione di tutte le img all'interno del box dx
+const itemsBoxDx = document.getElementsByClassName("box_img_dx")
+console.log(itemsBoxDx[2])
 
 // Prima immagine visibile
 let activeItem = 0;
 itemsArrey[activeItem].classList.add("active");
+// Prima immagine oscuprata
+itemsBoxDx[activeItem].classList.add("opacity")
 
 // bottone click 
 let btnCarosel = document.getElementById("my_btn_next");
 btnCarosel.addEventListener("click",
     () => {
         if (activeItem < imgArrey.length -1){
+            itemsBoxDx[activeItem].classList.remove("opacity")
             itemsArrey[activeItem].classList.remove("active")
             activeItem++;
             itemsArrey[activeItem].classList.add("active")
+            itemsBoxDx[activeItem].classList.add("opacity")
         } else {
             // Carosello infinito
+            itemsBoxDx[activeItem].classList.remove("opacity")
             itemsArrey[activeItem].classList.remove("active")
             activeItem = 0
             itemsArrey[activeItem].classList.add("active")
+            itemsBoxDx[activeItem].classList.add("opacity")
         }
     }
 )
+
+
+
+
+
+
